0a57866d95f6ab0fc7d0bd4430a0201b9b206962
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-04-17 09:41:21 +0700

    Added tests

---
+++ test/itb/arkavquarium/GuppyTest.java
@@ -0,0 +1,30 @@
+package itb.arkavquarium;
+
+import static org.junit.Assert.*;
+
+public class GuppyTest {
+
+    @org.junit.Test
+    public void move() {
+    }
+
+    @org.junit.Test
+    public void updateState() {
+    }
+
+    @org.junit.Test
+    public void updateProgress() {
+    }
+
+    @org.junit.Test
+    public void dead() {
+    }
+
+    @org.junit.Test
+    public void dropCoin() {
+    }
+
+    @org.junit.Test
+    public void eat() {
+    }
+}
