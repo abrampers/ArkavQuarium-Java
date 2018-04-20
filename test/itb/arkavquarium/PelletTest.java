966f62f53dd31ffbb66c9939935f8400ab1dcad3
diwangs <27280516+diwangs@users.noreply.github.com>
2018-04-20 20:55:25 +0700

    Implemented Coin and Pellet JUnit test

--- test/itb/arkavquarium/PelletTest.java
+++ test/itb/arkavquarium/PelletTest.java
@@ -5,20 +5,36 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PelletTest {
+  // Stub Aquarium
+  private Aquarium a = new Aquarium(0, 0, 100, 100);
+
+  /*
+   This is a test just for the pellet mechanics.
+   For a test where the pellet gets eaten by a Guppy, see Guppy test.
+  */
 
   @Test
-  public void move() {
+  public void fallTest() {
+    Pellet p = new Pellet(50, 50, a);
+    a.getContentPellet().add(p);
+    a.updateState(5.0); // this will move the pellet
+    assertTrue("Pellet is not falling", p.getY() > 50);
   }
 
   @Test
-  public void updateState() {
+  public void xTest() {
+    Pellet p = new Pellet(50, 50, a);
+    a.getContentPellet().add(p);
+    a.updateState(5.0);
+    assertEquals("Pellet's abcissa changed", 50, p.getX(), 0.01);
   }
 
   @Test
-  public void updateProgress() {
-  }
-
-  @Test
-  public void dead() {
+  public void deadTest() {
+    Pellet p = new Pellet(50, 99, a);
+    a.getContentPellet().add(p);
+    a.updateState(100.0);
+    // At this time, the pellet is in the bottom of the aquarium
+    assertTrue("Pellet goes divine", a.getContentPellet().isEmpty());
   }
 }
