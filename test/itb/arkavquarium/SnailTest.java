0e99d66c15aca5512b027202ba24fe31f6b0d0c7
diwangs <27280516+diwangs@users.noreply.github.com>
2018-04-21 00:39:33 +0700

    Implemented Snail JUnit test and fixed Snail swimming bug

--- test/itb/arkavquarium/SnailTest.java
+++ test/itb/arkavquarium/SnailTest.java
@@ -5,20 +5,39 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SnailTest {
+  // Stub Aquarium
+  private Aquarium a = new Aquarium(0, 0, 100, 100);
 
   @Test
-  public void move() {
+  public void chaseCoinTest() {
+    Coin c = new Coin(45, a.getYMax(), 50, a);
+    Snail s = new Snail(a);
+    s.setX(15);
+    a.getContentCoin().add(c);
+    a.getContentSnail().add(s);
+    a.updateState(0.2);
+    assertEquals("Snail didn't correctly chase coin", State.movingRight,  s.getState());
   }
 
   @Test
-  public void updateState() {
+  public void yTest() {
+    Coin c = new Coin(45, a.getYMax(), 50, a);
+    Snail s = new Snail(a);
+    s.setX(15);
+    a.getContentCoin().add(c);
+    a.getContentSnail().add(s);
+    a.updateState(0.2);
+    assertEquals("Somehow Snail can swim",  a.getYMax(), s.getY(), 0.01);
   }
 
   @Test
-  public void updateProgress() {
-  }
-
-  @Test
-  public void dead() {
+  public void eatCoinTest() {
+    Coin c = new Coin(45, a.getYMax(), 50, a);
+    Snail s = new Snail(a);
+    s.setX(50);
+    a.getContentCoin().add(c);
+    a.getContentSnail().add(s);
+    a.updateState(1);
+    assertTrue("Coin is not being eaten", a.getContentCoin().isEmpty());
   }
 }
