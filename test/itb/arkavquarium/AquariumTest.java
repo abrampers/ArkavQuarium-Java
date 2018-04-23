a40718252b7727f2b438597b194bc7d5b262efd0
yusufrahmatp <yusufrahmatp@gmail.com>
2018-04-23 17:25:39 +0700

    Move aquarium object declaration inside method to avoid collision between testing in unit testing for Aquarium Class

--- test/itb/arkavquarium/AquariumTest.java
+++ test/itb/arkavquarium/AquariumTest.java
@@ -1,27 +1,25 @@
 package itb.arkavquarium;
 
 import java.util.Iterator;
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AquariumTest {
-  /* Aquarium object for testing */
-  private Aquarium a = new Aquarium(0, 0, 100, 100);
-
   /*
   Test for Aquarium's mechanics
    */
 
   @Test
   public void updateState() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     a.updateState(50);
     assertEquals("Current time not incremented properly", 50, a.getCurrTime(), 0.01);
   }
 
   @Test
   public void createPiranha() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     int initLength = a.getContentPiranha().getLength();
     a.createPiranha();
     assertTrue("Piranha creation failed", a.getContentPiranha().getLength() > initLength);
@@ -29,6 +27,7 @@ public class AquariumTest {
 
   @Test
   public void createGuppy() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     int initLength = a.getContentGuppy().getLength();
     a.createGuppy();
     assertTrue("Guppy creation failed", a.getContentGuppy().getLength() > initLength);
@@ -36,6 +35,7 @@ public class AquariumTest {
 
   @Test
   public void createSnail() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     int initLength = a.getContentSnail().getLength();
     a.createSnail();
     assertTrue("Snail creation failed", a.getContentSnail().getLength() > initLength);
@@ -43,6 +43,7 @@ public class AquariumTest {
 
   @Test
   public void createPellet() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     int initLength = a.getContentPellet().getLength();
     a.createPellet(10, 10);
     assertTrue("Pellet creation failed", a.getContentPellet().getLength() > initLength);
@@ -50,6 +51,7 @@ public class AquariumTest {
 
   @Test
   public void createCoin() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     int initLength = a.getContentCoin().getLength();
     a.createCoin(15, 15, 100);
     assertTrue("Coin creation failed", a.getContentCoin().getLength() > initLength);
@@ -57,6 +59,7 @@ public class AquariumTest {
 
   @Test
   public void deletePiranha() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     Piranha p = null;
     Iterator<Piranha> piranhaIterator = a.getContentPiranha().iterator();
     a.createPiranha();
@@ -77,6 +80,7 @@ public class AquariumTest {
 
   @Test
   public void deleteGuppy() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     Guppy g = null;
     Iterator<Guppy> guppyIterator = a.getContentGuppy().iterator();
     a.createGuppy();
@@ -97,6 +101,7 @@ public class AquariumTest {
 
   @Test
   public void deleteSnail() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     Snail s = null;
     Iterator<Snail> snailIterator = a.getContentSnail().iterator();
     a.createSnail();
@@ -117,6 +122,7 @@ public class AquariumTest {
 
   @Test
   public void deletePellet() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     Pellet p = null;
     Iterator<Pellet> pelletIterator = a.getContentPellet().iterator();
     a.createPellet(10, 10);
@@ -137,6 +143,7 @@ public class AquariumTest {
 
   @Test
   public void deleteCoin() {
+    Aquarium a = new Aquarium(0, 0, 100, 100);
     Coin c = null;
     Iterator<Coin> coinIterator = a.getContentCoin().iterator();
     a.createCoin(15, 15, 100);
