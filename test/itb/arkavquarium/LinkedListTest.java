d254bb06fb004afa3f71653f4824179ceebe9856
yusufrahmatp <yusufrahmatp@gmail.com>
2018-04-23 17:22:27 +0700

    Add unit testing implementation for class LinkedList

--- test/itb/arkavquarium/LinkedListTest.java
+++ test/itb/arkavquarium/LinkedListTest.java
@@ -2,23 +2,66 @@ package itb.arkavquarium;
 
 import static org.junit.Assert.*;
 
+import java.util.Iterator;
 import org.junit.Test;
 
 public class LinkedListTest {
 
+  /*
+  Test for LinkedList's methods
+   */
+
   @Test
   public void isEmpty() {
+    LinkedList<Integer> l = new LinkedList<Integer>();
+    assertTrue("Initialization returns non-empty list", l.isEmpty());
+    l.add(5);
+    assertFalse("Element addition doesn't make list non-empty", l.isEmpty());
   }
 
   @Test
   public void add() {
+    LinkedList<Integer> l = new LinkedList<Integer>();
+    Iterator<Integer> intIterator = l.iterator();
+    boolean found = false;
+    l.add(10);
+    while (intIterator.hasNext()) {
+      if (intIterator.next() == 10) {
+        found = true;
+      }
+    }
+    assertTrue("Element addition failed", found);
   }
 
   @Test
   public void remove() {
+    LinkedList<Integer> l = new LinkedList<Integer>();
+    Iterator<Integer> intIterator = l.iterator();
+    l.add(13);
+    l.remove(13);
+    while (intIterator.hasNext()) {
+      if (intIterator.next() == 13) {
+        fail("Element removal failed");
+      }
+    }
   }
 
   @Test
   public void iterator() {
+    LinkedList<Integer> l = new LinkedList<Integer>();
+    Iterator<Integer> intIterator = l.iterator();
+    int i;
+    int[] elementExample = new int[5];
+    for (i = 0; i < elementExample.length; i++) {
+      l.add(i);
+      elementExample[i] = i;
+    }
+    i = 0;
+    while (intIterator.hasNext()) {
+      if (intIterator.next() != elementExample[i]) {
+        fail("Element called by iterator is not equal to the data inserted");
+      }
+      i++;
+    }
   }
 }
