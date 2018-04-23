aa40791bb0a2b6a581759919f0f4f6ce3d05c6ab
diwangs <27280516+diwangs@users.noreply.github.com>
2018-04-24 02:47:10 +0700

    Added Piranha and Guppy JUnit test bre

--- test/itb/arkavquarium/GuppyTest.java
+++ test/itb/arkavquarium/GuppyTest.java
@@ -1,32 +1,58 @@
 package itb.arkavquarium;
 
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GuppyTest {
+  // Stub Aquarium
+  private Aquarium a = new Aquarium(0, 0, 100, 100);
 
   @Test
-  public void move() {
+  public void chasePelletTest() {
+    Pellet p = new Pellet(50, 50, a);
+    Guppy g = new Guppy(a);
+    g.setX(25);
+    g.setY(50);
+    g.setHungry(true);
+    a.getContentPellet().add(p);
+    a.getContentGuppy().add(g);
+    a.updateState(0.1);
+    // if the test is negative, it's definitely wrong
+    // if the test is positive, it's probably right. Test it repeatedly
+    assertTrue("Guppy turns left, while the pellet is in the right", g.getX() > 25);
   }
 
   @Test
-  public void updateState() {
+  public void eatPelletTest() {
+    Pellet p = new Pellet(50, 50, a);
+    Guppy g = new Guppy(a);
+    g.setX(35);
+    g.setY(50);
+    g.setHungry(true);
+    a.getContentPellet().add(p);
+    a.getContentGuppy().add(g);
+    a.updateState(0.1);
+    assertTrue("Guppy didn't eat the pellet", a.getContentPellet().isEmpty());
   }
 
   @Test
-  public void updateProgress() {
+  public void dropCoinTest() {
+    Guppy g = new Guppy(a);
+    a.getContentGuppy().add(g);
+    a.updateState(Constants.GUPPY_COIN_INTERVAL + 1);
+    assertTrue("Guppy didn't produce coin after the interval", !a.getContentCoin().isEmpty());
   }
 
   @Test
-  public void dead() {
+  public void deadTest() {
+    Guppy g = new Guppy(a);
+    g.setHungry(true);
+    a.getContentGuppy().add(g);
+    a.updateState(1000);
+    System.out.println(a.getContentGuppy().getLength());
+    assertTrue("Guppy goes divine", a.getContentGuppy().isEmpty());
   }
 
-  @Test
-  public void dropCoin() {
-  }
-
-  @Test
-  public void eat() {
-  }
 }
--- test/itb/arkavquarium/PiranhaTest.java
+++ test/itb/arkavquarium/PiranhaTest.java
@@ -5,28 +5,62 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PiranhaTest {
+  // Stub Aquarium
+  private Aquarium a = new Aquarium(0, 0, 100, 100);
 
   @Test
-  public void move() {
+  public void chaseGuppyTest() {
+    Piranha p = new Piranha(a);
+    p.setX(25);
+    p.setY(50);
+    Guppy g = new Guppy(a);
+    g.setX(50);
+    g.setY(50);
+    p.setHungry(true);
+    a.getContentPiranha().add(p);
+    a.getContentGuppy().add(g);
+    a.updateState(0.1);
+    // if the test is negative, it's definitely wrong
+    // if the test is positive, it's probably right. Test it repeatedly
+    assertTrue("Piranha turns left, while the Guppy is in the right", p.getX() > 25);
   }
 
   @Test
-  public void updateState() {
+  public void eatGuppyTest() {
+    Piranha p = new Piranha(a);
+    p.setX(35);
+    p.setY(50);
+    Guppy g = new Guppy(a);
+    g.setX(50);
+    g.setY(50);
+    p.setHungry(true);
+    a.getContentPiranha().add(p);
+    a.getContentGuppy().add(g);
+    a.updateState(0.1);
+    assertTrue("Piranha didn't eat the Guppy", a.getContentGuppy().getLength() == 1);
   }
 
   @Test
-  public void updateProgress() {
+  public void dropCoinTest() {
+    Piranha p = new Piranha(a);
+    p.setX(35);
+    p.setY(50);
+    Guppy g = new Guppy(a);
+    g.setX(50);
+    g.setY(50);
+    p.setHungry(true);
+    a.getContentPiranha().add(p);
+    a.getContentGuppy().add(g);
+    a.updateState(0.1);
+    assertTrue("Piranha didn't produce coin after eating Guppy", !a.getContentCoin().isEmpty());
   }
 
   @Test
-  public void dead() {
-  }
-
-  @Test
-  public void dropCoin() {
-  }
-
-  @Test
-  public void eat() {
+  public void deadTest() {
+    Piranha p = new Piranha(a);
+    p.setHungry(true);
+    a.getContentPiranha().add(p);
+    a.updateState(1000);
+    assertTrue("Piranha goes divine", a.getContentPiranha().isEmpty()); // ?
   }
 }
