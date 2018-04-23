ac70cdb0a6fb4ab0ce9bf1c8a48b4136723717a8
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-04-23 08:58:45 +0700

    Added LinkedList test

---
+++ test/itb/arkavquarium/LinkedListTest.java
@@ -0,0 +1,24 @@
+package itb.arkavquarium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+
+  @Test
+  public void isEmpty() {
+  }
+
+  @Test
+  public void add() {
+  }
+
+  @Test
+  public void remove() {
+  }
+
+  @Test
+  public void iterator() {
+  }
+}
